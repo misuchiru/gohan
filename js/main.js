d5a28efbc9b91315065352333d3b871537bfade1
misuchiru <perryhaha@gmail.com>
2017-04-13 14:06:13 +0800

    v13

--- js/main.js
+++ js/main.js
@@ -17,5 +17,16 @@ $(function(){
 	$(document).click(function(e){
 		$('.headerMenu').fadeOut();
 		e.stopPropagation();
-	})
-})
+	});
+
+	/*menu active*/
+	var m_urlName = ['heart', 'smart', 'beauty', 'delicious', 'wakeup', 'goodTaste', 'jiming', 'item', 'spicy', 'local'];
+	var m_menuLen = m_urlName.length;
+	var m_url= document.location.pathname.match(/[^\/]+$/)[0];
+	for(var i = 0 ; i < m_menuLen ; ++i){
+		if(m_url.indexOf(m_urlName[i]) != -1){
+			console.log('array = ' + m_urlName[i] + "  m_url = " + m_url);
+			$('a[href = "' + m_urlName[i] + '.html"]').addClass('active');
+		};
+	};
+});
