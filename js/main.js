0382c2be0063b383040ab9200e46abcd5671961d
perry <perry@perrydeiMac.local>
2017-04-26 22:40:57 +0800

    v16

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+	var testCount = 0;
+	var randomTest = 0;
 	$('.headerMenuBtn').click(function(e){
 		$('.headerMenu').fadeToggle();
 		e.stopPropagation();
@@ -8,11 +10,26 @@ $(function(){
 			$('.testPic').fadeIn()
 		});
 	});*/
-	$('.mainbtn3').click(function(){
+	/*$('.mainbtn3').click(function(){
 		$(this).fadeOut('slow',function(){
 			$('.testPic').hide()
 			$('.ansPic').fadeIn()
 		});
+	});*/
+	$('div[class*="mainbtn"]').click(function(){
+		if($(this).hasClass('active')){
+			$(this).removeClass('active');
+			return;
+		}else{
+			testCount ++;
+			$(this).addClass('active');
+		}
+		if(testCount == 3){
+			testCount = 0;
+			$('div[class*="mainbtn"]').removeClass('active');
+			randomTest = Math.floor(Math.random()*8)+1;
+			window.location.href = './result' + randomTest + '.html'
+		};
 	});
 	$(document).click(function(e){
 		$('.headerMenu').fadeOut();
